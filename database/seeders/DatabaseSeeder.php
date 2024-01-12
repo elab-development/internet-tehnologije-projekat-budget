91cdc3ba94eac04beae9bf99d3ca58c0f31ad43d
Uros20170112 <57675991+Uros20170112@users.noreply.github.com>
2024-01-12 15:21:34 +0100

    added API routes and DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Friend;
+use App\Models\User;
+use App\Models\Voyage;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Friend::truncate();
+        Voyage::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(1)->create();
+        Friend::factory(5)->create();
+        Voyage::factory(20)->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Authorization\APIController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\FriendController;
+use App\Http\Controllers\VoyageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/friends', [FriendController::class, 'index']);
+Route::get('/friends/{id}', [FriendController::class, 'show']);
+
+Route::get('/voyages', [VoyageController::class, 'index']);
+Route::get('/voyages/p', [VoyageController::class, 'indexPaginate']);
+Route::get('/voyages/{id}', [VoyageController::class, 'show']);
+
+Route::get('/expenses', [ExpenseController::class, 'index']);
+Route::get('/expenses/{id}', [ExpenseController::class, 'show']);
+
+Route::post('/register', [APIController::class, 'register']);
+Route::post('/login', [APIController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    Route::resource('/friends', FriendController::class)
+        ->only(['store', 'update', 'destroy']);
+
+    Route::resource('/voyages', VoyageController::class)
+        ->only(['store', 'update', 'destroy']);
+
+    Route::resource('/expenses', ExpenseController::class)
+        ->only(['store', 'update', 'destroy']);
+
+    Route::post('/logout', [APIController::class, 'logout']);
 });
